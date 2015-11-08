5f78f701a7cf2d5b920369bfe7cbf2016ca0ba11
Heitor Lessa <heitor.lessa@hotmail.com>
2015-11-08 22:20:18 +0000

    [+] Added Config/SNS structs, SNS feature and fixed unexpected multiple messages due to wrong channel reading

--- example/main.go
+++ example/main.go
@@ -3,26 +3,87 @@ package main
 import (
 	"ec2SpotNotify"
 	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/sns"
 	"log"
 )
+
+type Config struct {
+	Region string
+	SNS
+}
+
+type SNS struct {
+	Topic   string
+	Subject string
+	Message string
+}
 
 func main() {
 
 	log.Println("Looking up Instance Metadata....")
-	notification, err := ec2SpotNotify.GetNotificationTime()
+	notification, instance, err := ec2SpotNotify.GetNotificationTime()
 	if err != nil {
 		fmt.Errorf("Ooops! Something went terribly wrong: ", err)
 	}
 
-	// as notification may take a while to be injected on EC2 Metadata - Read from channel provided
-	for {
-		select {
-		case <-notification:
-			log.Println("received termination -> ", notification)
-		default:
-			log.Println("Not yet...")
-		}
+	config := loadConfig(&Config{
+		Region: "<AWS Region>",
+		SNS: SNS{
+			Topic:   "arn:aws:sns:<region>:<accountID>:<topic>",
+			Subject: "Spot Termination notification",
+			Message: instance,
+		},
+	})
+
+	// as notification may take a while to be injected on EC2 Metadata - Read from channel provided with range
+	for timestamp := range notification {
+		log.Println("Received notification -> ", timestamp)
+		publishSNS(config)
 	}
+
 	log.Println("Doing something else...")
 	// do something about it ;)
+}
+
+// Need to implement config check + read from env if file is not present or if 'nil' passed as argument
+func loadConfig(c *Config) *Config {
+
+	log.Println("Gathering configuration....")
+
+	return &Config{
+		Region: c.Region,
+		SNS: SNS{
+			Topic:   c.SNS.Topic,
+			Subject: c.SNS.Subject,
+			Message: c.SNS.Message,
+		},
+	}
+}
+
+func publishSNS(c *Config) {
+
+	// AWS initialization
+	config := aws.NewConfig().WithRegion(c.Region)
+	client := sns.New(config)
+
+	// SNS params prior to publish message to topic
+	// ref: http://docs.aws.amazon.com/sdk-for-go/api/service/sns/SNS.html#Publish-instance_method
+	params := &sns.PublishInput{
+		Message: aws.String(string(c.SNS.Message)),
+		MessageAttributes: map[string]*sns.MessageAttributeValue{
+			"Key": {
+				DataType:    aws.String("String"),
+				StringValue: aws.String("String"),
+			},
+		},
+		Subject:  aws.String(c.SNS.Subject),
+		TopicArn: aws.String(c.SNS.Topic),
+	}
+
+	_, err := client.Publish(params)
+	if err != nil {
+		log.Fatal(err.Error())
+		return
+	}
 }
