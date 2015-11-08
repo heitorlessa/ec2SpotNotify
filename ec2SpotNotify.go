4512bd8b73b724f9735020e56cca01c571a638d5
Heitor Lessa <heitor.lessa@hotmail.com>
2015-11-08 22:18:57 +0000

    [+] Added instance details onto response to be used within SNS message

--- ec2SpotNotify.go
+++ ec2SpotNotify.go
@@ -14,19 +14,20 @@ const (
 	RequestFound               string        = "200 OK"
 	RequestTimeoutSeconds      time.Duration = 150 * time.Millisecond
 	URLTerminationNotification string        = "http://169.254.169.254/latest/meta-data/spot/termination-time"
+	URLInstanceDetails         string        = "http://169.254.169.254/latest/dynamic/instance-identity/document"
 	TimeFormat                 string        = "2006-01-02T15:04:05Z07:00" // RFC 3339
 	TimeThresholdInterval      time.Duration = 3 * time.Second
 )
 
 // GetNotificationTime is a public function that returns a time based channel and error
 // Returned channel should be read to identify when Spot Instance will be terminated so actions can be taken
-func GetNotificationTime() (chan time.Time, error) {
+func GetNotificationTime() (chan time.Time, string, error) {
 	notifyChan := make(chan time.Time, 1)
 
 	// quick error check (timeout error if not an EC2 instance)
-	if _, err := lookupInstanceMetadata(); err != nil {
+	_, instanceDetails, err := lookupInstanceMetadata()
+	if err != nil {
 		log.Fatal(err)
-		os.Exit(1)
 	}
 
 	// run goroutine and keep it running until data is available
@@ -39,7 +40,7 @@ func GetNotificationTime() (chan time.Time, error) {
 		// listen to "ticks" and do something about it
 		for _ = range ticker.C {
 			// find out if data is available and store it on notification
-			notification, err := lookupInstanceMetadata()
+			notification, _, err := lookupInstanceMetadata()
 
 			if err != nil {
 				log.Fatal(err)
@@ -54,23 +55,23 @@ func GetNotificationTime() (chan time.Time, error) {
 		}
 	}()
 
-	return notifyChan, nil
+	return notifyChan, instanceDetails, nil
 }
 
 // lookupInstanceMetadata looks at EC2 Instance Metadata for URLTerminationNotification to extract when Spot Instance will be terminated
 // Returns timestamp and error so it can be worked by GetNotificationTime
 // While the instance is not marked for termination EC2 will return HTTP 404
-func lookupInstanceMetadata() (timestamp time.Time, err error) {
+func lookupInstanceMetadata() (timestamp time.Time, instance string, err error) {
 
 	// return a Zero timestamp if termination notification is not ready
 	// ref: ref: https://golang.org/pkg/time/#Time.IsZero
-	ZeroTimestamp, _ := time.Parse(TimeFormat, string(""))
+	ZeroTimestamp, _ := time.Parse(TimeFormat, "")
 
 	// set shorter timeout as default is way too high for this operation
-	request := gorequest.
+	req := gorequest.
 		New().
 		Timeout(RequestTimeoutSeconds)
-	resp, body, errs := request.Get(URLTerminationNotification).End()
+	resp, body, errs := req.Get(URLTerminationNotification).End()
 
 	// return error if request times out
 	if errs != nil {
@@ -78,18 +79,25 @@ func lookupInstanceMetadata() (timestamp time.Time, err error) {
 		return
 	}
 
+	// make another request only for instance details (eg AZ, IP, AMI, etc)
+	req2 := gorequest.
+		New().
+		Timeout(RequestTimeoutSeconds)
+
+	_, instanceDetails, _ := req2.Get(URLInstanceDetails).End()
+
 	switch resp.Status {
 	case RequestNotFoundError:
 		log.Println("[-] Not found yet....")
-		return ZeroTimestamp, nil
+		return ZeroTimestamp, "", nil
 		fallthrough
 	case RequestFound:
 		notification, _ := time.Parse(TimeFormat, body)
-		return notification, nil
+		return notification, instanceDetails, nil
 		fallthrough
 	default:
 		fmt.Errorf("[!] Received a non-compliant status: %s ", resp.Status)
-		return ZeroTimestamp, nil
+		return ZeroTimestamp, "", nil
 	}
 	return
 }
